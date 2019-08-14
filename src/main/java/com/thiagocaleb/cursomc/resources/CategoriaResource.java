0758333d88bb1c4a9e326373a9a087aa42b8b683
Thiago Caleb <thiagodiniz811@gmail.com>
2019-08-14 09:59:54 -0300

    criando operaçao de instaciaçao

--- src/main/java/com/thiagocaleb/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/thiagocaleb/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,9 @@
 package com.thiagocaleb.cursomc.resources;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,18 +11,30 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.thiagocaleb.cursomc.domain.Categoria;
+import com.thiagocaleb.cursomc.repositories.CategoriaRepository;
 import com.thiagocaleb.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
-public class CategoriaResource {
+public class CategoriaResource implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaService service;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 }

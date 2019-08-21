95a01060a6dc3b9bb58a3c095bdd6e50d67887b5
Thiago Caleb <thiagodiniz811@gmail.com>
2019-08-20 22:53:37 -0300

    Estado e Cidade

--- src/main/java/com/thiagocaleb/cursomc/CursomcApplication.java
+++ src/main/java/com/thiagocaleb/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.thiagocaleb.cursomc.domain.Categoria;
+import com.thiagocaleb.cursomc.domain.Cidade;
+import com.thiagocaleb.cursomc.domain.Estado;
 import com.thiagocaleb.cursomc.domain.Produto;
 import com.thiagocaleb.cursomc.repositories.CategoriaRepository;
+import com.thiagocaleb.cursomc.repositories.CidadeRepository;
+import com.thiagocaleb.cursomc.repositories.EstadoRepository;
 import com.thiagocaleb.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -17,9 +21,16 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -28,20 +39,29 @@ public class CursomcApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
-		
+
 		Produto p1 = new Produto(null, "Computador", 200.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
-		
-		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "Sao Paulo");
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "Sao Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 	}
 
 }
---
+++ src/main/java/com/thiagocaleb/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,12 @@
+package com.thiagocaleb.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.thiagocaleb.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+	
+}
